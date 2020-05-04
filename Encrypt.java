7e7cd173bcabe58f157c3e0a056a18e7498ff63a
jcampos302 <jorgeecampos@lewisu.edu>
2020-05-04 17:16:25 -0500

    Adding files

--- Encrypt.java
+++ Encrypt.java
@@ -1,150 +1,173 @@
 // Jorge Campos Test File
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.SwingUtilities;
-
-import java.io.*;
-
-import java.awt.Container; 
-import java.awt.GridLayout;
-import java.awt.BorderLayout;
-
+import java.awt.Container;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.Signature;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-
-
-
-
-class EncryptFrame extends JFrame implements ActionListener{
+class EncryptFrame extends JFrame implements ActionListener {
 
     JFileChooser fc;
-    JTextArea log;
+    JTextArea Filelog, shiftText, statusLog;
+    JLabel shiftLabel;
     File file;
-    JButton Encrypt,Decrypt,OpenFile;
-
-    Signature sign;
-    KeyPairGenerator keyPairGen;
-    KeyPair pair;
-    Cipher cipher;
-
+    JButton Encrypt, Decrypt, OpenFile;
+    String NewFile;
 
     public EncryptFrame() {
-        setSize(400,400);
+        setSize(400, 400);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container contentPane = getContentPane();
 
         Encrypt = new JButton("Encrypt");
-        Encrypt.setBounds(50,150,90,30);
+        Encrypt.setBounds(50, 200, 90, 30);
         Encrypt.addActionListener(this);
-        
+
         Decrypt = new JButton("Decrypt");
-        Decrypt.setBounds(270, 150, 90, 30);
+        Decrypt.setBounds(270, 200, 90, 30);
         Decrypt.addActionListener(this);
 
+        shiftLabel = new JLabel("Enter a Shift # (1-25)");
+        shiftLabel.setBounds(25, 125, 225, 30);
+        shiftLabel.setFont(new Font("TimesRoman", Font.PLAIN, 18));
+        
+        shiftText = new JTextArea(5,20);
+        shiftText.setFont(new Font("TimesRoman", Font.PLAIN, 18));
+        shiftText.setBounds(250, 125, 50, 30);
+        shiftText.setEditable(true);
+
         OpenFile = new JButton("Open File...");
-        OpenFile.setBounds(25,50,125,30);
+        OpenFile.setBounds(25, 50, 125, 30);
         OpenFile.addActionListener(this);
 
-        log = new JTextArea(5,20);
-        log.setBounds(150,50,225,30);
-        log.setEditable(false);
-        
+        Filelog = new JTextArea(5, 20);
+        Filelog.setBounds(150, 50, 225, 30);
+        Filelog.setFont(new Font("TimesRoman", Font.PLAIN, 12));
+        Filelog.setEditable(false);
+
+        statusLog = new JTextArea(5,20);
+        statusLog.setBounds(35,275,325,30);
+        statusLog.setFont(new Font("TimesRoman", Font.PLAIN, 18));
+        statusLog.setEditable(false);
 
         contentPane.add(Encrypt);
         contentPane.add(Decrypt);
         contentPane.add(OpenFile);
-        contentPane.add(log);
+        contentPane.add(Filelog);
+        contentPane.add(shiftText);
+        contentPane.add(shiftLabel);
+        contentPane.add(statusLog);
 
-
-        
-
+        statusLog.setText("Welcome to Encrypt\n");
 
     }
+
+    public void Encrypt() {
+        statusLog.setText("Starting Encryption on: "+ file.getName());
+        try{
+            int shift = Integer.parseInt(shiftText.getText());
+            
+            try{
+                Scanner input = new Scanner(file);
+                PrintStream output = new PrintStream(new File("Encrypted_"+NewFile));
+                while(input.hasNextLine()){
+                    output.println(caesar(input.nextLine(),shift));
+                }
+                input.close();
+                statusLog.setText("Created file Encrypted_"+NewFile);
+            } catch (IOException e){
+                statusLog.setText("Error File is unable to Encrypt.\n");
+            }
+        } catch (NumberFormatException e){
+            statusLog.setText("Error Shift # Not Accepted.\n");
+        }
+    }
+
+    public void Decrypt() {
+        statusLog.setText("Starting Decryption on: "+ file.getName());
+        try{
+            int shift = 26 - Integer.parseInt(shiftText.getText());
+            try{
+                Scanner input = new Scanner(file);
+                PrintStream output = new PrintStream(new File("DecryptFile_"+NewFile));
+                while(input.hasNextLine()){
+                    output.println(caesar(input.nextLine(),shift));
+                }
+                input.close();
+                statusLog.setText("Created File DecryptedFile_"+NewFile);
+            } catch (IOException e){
+                statusLog.setText("Error File is unable to Decrypt\n");
+            }
+        } catch (NumberFormatException e){
+            statusLog.setText("Error Shift # Not Accepted.\n");
+        }
+    }
+
+    public static String caesar(String text, int shift) {
+        shift %= 26;
+        if (shift == 0) return text;
+        StringBuilder sb = new StringBuilder(text.length());
+        for (int i = 0; i < text.length(); i++) {
+            int c = text.charAt(i);
+
+            if (c >= 'A' && c <= 'Z') {
+                c += shift;
+                if (c > 'Z') c -= 26;
+            } else if (c >= 'a' && c <= 'z') {
+                c += shift;
+                if (c > 'z') c -= 26;
+            }
+
+            sb.append((char) c);
+        }
+        return sb.toString();
+    }
+     
     public void OpenFile(){
         int returnVal = fc.showOpenDialog(EncryptFrame.this);
 
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                file = fc.getSelectedFile();
-                log.append("Opening: " +file.getName() +".\n");
-            } else {
-                log.append("Open command cancelled by user.\n");
-            }
-            log.setCaretPosition(log.getDocument().getLength());
-            System.out.println("Done here");
-            
-    }
-
-    public void GenKey(){
-        try{
-            sign = Signature.getInstance("SHA256withRSA");
-
-            keyPairGen = KeyPairGenerator.getInstance("RSA");
-    
-            keyPairGen.initialize(2048);
-    
-            pair = keyPairGen.generateKeyPair(); 
-        }catch(Exception e){
-            System.out.println("Error");
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            file = fc.getSelectedFile();
+            Filelog.setText("Opening: " +file.getName() +".\n");
+        } else {
+            Filelog.setText("Open Command Cancelled by user.\n");
         }
-    }
-
-    public void Encryption(){
-       try{
-
-        cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-
-        cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
-        
-        byte[] input = file.toString().getBytes();
-        cipher.update(input);
-
-        byte[] cipherText = cipher.doFinal();	 
-        System.out.println(new String(cipherText, "UTF8"));
-
-        }catch(Exception e){
-            System.out.println("Error :(");
-        }
-    }
-
-    public void Decryption(){
-        try{
-     
-        } catch(Exception e){
-            System.out.println("Error");
-        }
+        Filelog.setCaretPosition(Filelog.getDocument().getLength());
+        NewFile = file.getName();    
     }
 
     public void actionPerformed(ActionEvent e){
         fc = new JFileChooser();
-        GenKey();
 
         if (e.getSource() == OpenFile) {
             OpenFile();
         
         }
         else if (e.getSource() == Encrypt) {
-            System.out.println(file.getName());
-            Encryption();
-
+            try{
+                Encrypt();
+            } catch (NullPointerException e1){
+                statusLog.setText("Error Please Select a File.\n");
+            }
         }
         else if (e.getSource() == Decrypt) {
-
+            try{
+            Decrypt();
+            } catch (NullPointerException e1){
+                statusLog.setText("Error Please Select a File.\n");
+            }
         }
-
-
 
     }
 }
@@ -153,11 +176,8 @@ public class Encrypt {
 
     public static void main(String[] args) throws Exception{
 
-        System.out.println("File Encript Starting");
-
         EncryptFrame EncryptFrame = new EncryptFrame();
         EncryptFrame.setVisible(true);
-
-
+        
     }
 }
